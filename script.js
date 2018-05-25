510a23c95e28e241680a7e87694c75593aff8ff0
iosonosempreio <tommaso.elli@gmail.com>
2018-05-25 21:39:35 +0200

    ok

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-// const toPoints = SVGPoints.toPoints
-// const toPath = SVGPoints.toPath
+d3.select('#contourdown')
+    .style('width','100%')
+    .style('height',0)
+    .style('padding-bottom', 'calc(100% / 1.5)')
 
 opentype.load('fonts/WorkSans-Hairline.otf', function(err, font) {
     if (err) {
         alert('Font could not be loaded: ' + err);
     } else {
 
-        let w = d3.select('#contourdown').node().getBoundingClientRect().width,
-            h = d3.select('#contourdown').node().getBoundingClientRect().height;
+        let W = d3.select('#contourdown').node().getBoundingClientRect().width,
+            H = d3.select('#contourdown').node().getBoundingClientRect().height;
+
+        let w = 768,
+            h = 768 / 3 * 2;
 
         let delta_h = h;
+        let string;
+        var pathData;
 
-        let pointsAmount = 300, moveRandomly = 6;
+        // parameters set for 768 px width svg
+        let pointsAmount, pointsLetter = 75,
+            moveRandomly = 6,
+            bandwidth = 10;
 
-        let svg = d3.select('#contourdown').append('svg')
-            .attr('width', w)
-            .attr('height', h)
-        // .style('background-color', '#e4e4e4')
+        let svg = d3.select('#contourdown')
+            .append('svg')
+            .attr('viewBox', `0 0 ${w} ${h}`)
+            .attr('preserveAspectRatio', `xMidYMid meet`)
+            .style('width', '100%')
+            .style('height', 'auto')
+            .style('background-color', '#f5f5f5')
 
         let g = svg.append('g')
 
         let counterText = g.append('path')
             .attr('class', 'outlined-texts')
-            // .attr('fill', 'red')
             .style('opacity', '0')
 
         let points = g.selectAll('.points');
@@ -38,47 +50,29 @@ opentype.load('fonts/WorkSans-Hairline.otf', function(err, font) {
             .interpolator(d3.interpolateRainbow)
 
         var formatTime = d3.timeFormat("%H %M %S");
-        // formatTime(new Date); // "June 30, 2015"
-
-        let string;
-
         let idleInterval = setInterval(timerIncrement, 1000);
 
         // timerIncrement('RAW');
 
-        var pathData;
+
 
         function timerIncrement(txt) {
 
             string = txt ? txt : formatTime(new Date);
 
-            pointsAmount = string.length*75;
-
-            // string = 
-            // console.log(string);
-
-
-
-            if ( !pathData ) {
-                pathData = font.getPath(string, 25, delta_h, w / 4.1);
-                pathData = pathData.toPathData();
-                counterText.attr('d', pathData);
-            }
+            pointsAmount = string.length * pointsLetter;
 
             delta_h = h / 2 + counterText.node().getBBox().height / 2;
 
-            pathData = font.getPath(string, 25, delta_h, w / 4.1);
+            pathData = font.getPath(string, 50, 310, 188);
             pathData = pathData.toPathData();
             counterText.attr('d', pathData);
-
 
             let pointsData = [];
 
             for (i = 1; i <= pointsAmount; i++) {
-                // console.log(i)
                 let position = d3.select('.outlined-texts').node().getTotalLength() / pointsAmount * i;
                 let thisPoint = d3.selectAll('.outlined-texts').node().getPointAtLength(position);
-                // console.log(thisPoint);
                 thisPoint.id = i;
                 pointsData.push(thisPoint);
             }
@@ -91,7 +85,7 @@ opentype.load('fonts/WorkSans-Hairline.otf', function(err, font) {
                 }
             })
 
-            for (i = 1; i <= h/2; i++) {
+            for (i = 1; i <= h / 2; i++) {
                 let thisX = d3.randomUniform(0, w)();
                 let thisY = d3.randomUniform(0, h)()
                 let randomPoint = {
@@ -102,39 +96,23 @@ opentype.load('fonts/WorkSans-Hairline.otf', function(err, font) {
                 pointsData.push(randomPoint);
             }
 
-            // console.log(pointsData)
-
-            // points = points.data(pointsData, function(d) { return d.id; })
-
-            // points.exit().remove();
-
-            // points = points.enter().append('circle').classed('points', true)
-            //     .attr('fill', 'grey')
-            //     .attr('r', 1)
-            //     .style('opacity',1)
-            //     .merge(points)
-            //     .attr('cx', function(d) { return d.x })
-            //     .attr('cy', function(d) { return d.y })
-
             contours = contours.data(d3.contourDensity()
                 .x(function(d) { return d.x; return x(d.x); })
                 .y(function(d) { return d.y; return y(d.y); })
                 .size([w, h])
-                .bandwidth(13)
+                .bandwidth(bandwidth)
                 (pointsData),
                 function(d) { return d.id; })
 
             contours.exit().remove();
 
             contours = contours.enter().append("path")
-                .merge(contours)
                 .classed('contour', true)
+                .style('stroke-width', 0.4)
+                .style('fill', 'none')
+                .merge(contours)
                 .attr("d", d3.geoPath())
-                .attr("stroke", function(d) { return color(d.value); })
-
-            // contours.transition()
-            // .duration(500)
-            // .attr("d", d3.geoPath())
+                .style("stroke", function(d) { return color(d.value); })
 
         }
 
